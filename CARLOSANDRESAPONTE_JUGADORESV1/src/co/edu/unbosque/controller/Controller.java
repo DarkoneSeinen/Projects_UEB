1dc3d160f5f3ce1b0a895f9204ff984e67f016f6
eventos <eventos@UBSCOA402-39.unbosque.edu.co>
2023-10-25 14:25:44 -0500

    esfdsfs

--- CARLOSANDRESAPONTE_JUGADORESV1/src/co/edu/unbosque/controller/Controller.java
+++ CARLOSANDRESAPONTE_JUGADORESV1/src/co/edu/unbosque/controller/Controller.java
@@ -1,5 +1,21 @@
 package co.edu.unbosque.controller;
 
+import co.edu.unbosque.view.View;
+
 public class Controller {
+	
+	private View gui;
+
+	public Controller() {
+		
+		gui = new View(this);
+		gui.setVisible(true);
+		
+		// Agregar escuchadores de eventos
+//		BinariosFile gestor = new BinariosFile();
+////		gestor.escribirEmpleado(new EmpleadoDTO(1, "Empleado1", 46556.5654));
+//		gestor.leerEmpleado();
+		
+	}
 
 }
---
+++ CARLOSANDRESAPONTE_JUGADORESV1/src/co/edu/unbosque/view/View.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.view;
+
+import javax.swing.JFrame;
+
+import co.edu.unbosque.controller.Controller;
+
+public class View extends JFrame {
+
+	public View(Controller control) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
